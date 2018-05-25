647f5b926979da3a92b6b1b79e3b61ae58d0827e
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-05-25 18:22:27 +0200

    Implementacja rozkładu z el. głównym w kolumnie

--- ROZKLAD.m
+++ ROZKLAD.m
@@ -14,31 +14,59 @@ function [R, p, q] = ROZKLAD(A, s)
 %  p - wektor permutacji wierszy
 %  q - wektor permutacji kolumn
 
-  % TODO(kantoniak): Uzupełnić
   n = size(A, 1);
   R = A;
-  p = zeros(n, 1);
-  q = zeros(n, 1);
+  p = 1:n;
+  q = 1:n;
 
   switch s
-    case 0
-      
-      p = 1:n;
-      q = 1:n;
+    case 0 % Bez wyboru elementu głównego
 
-      R = A;
       for x=1:n-1
-        for y=x+1:n
-          R(y,x) = R(y,x)/R(x,x);
-          R(y,x+1:n) = R(y,x+1:n) - R(y,x)*R(x,x+1:n);
-        end
+        R = wyeliminuj_kolumne(R, x);
       end
 
-    case 1
-      disp('Opcja 1')
-    case 2
+
+    case 1 % Z elementem głównym w kolumnie
+      
+      for x=1:n-1
+        maks = x;
+        for i=x+1:n
+          if (abs(R(i,x)) > abs(R(x,x)))
+            maks = i;
+          end
+        end
+
+        if (maks > x)
+          tmp = R(x,:);
+          R(x,:) = R(maks,:);
+          R(maks,:) = tmp;
+
+          tmp = p(x);
+          p(x) = p(maks);
+          p(maks) = tmp;
+        end
+
+        R = wyeliminuj_kolumne(R, x);
+      end
+
+
+    case 2 % Z elementem głównym w macierzy
       disp('Opcja 2')
+
+
     otherwise
       error(sprintf('Opcja s=%d nie jest wspierana.', s))
   end
+end
+
+function [R] = wyeliminuj_kolumne(R, x)
+% wyeliminuj_kolumne wykonuje odejmowania, które eliminują x-tą kolumnę podmacierzy.
+
+  n = size(R, 1);
+  for y=x+1:n
+    R(y,x) = R(y,x)/R(x,x);
+    R(y,x+1:n) = R(y,x+1:n) - R(y,x)*R(x,x+1:n);
+  end
+
 end
