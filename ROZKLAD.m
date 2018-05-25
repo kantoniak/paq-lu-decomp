f3677bc10875a9a2ecfea7c879e3cab43f5bb28f
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-05-25 16:29:27 +0200

    Implementacja eliminacji Gaussa bez el. głównego

--- ROZKLAD.m
+++ ROZKLAD.m
@@ -19,4 +19,26 @@ function [R, p, q] = ROZKLAD(A, s)
   R = A;
   p = zeros(n, 1);
   q = zeros(n, 1);
+
+  switch s
+    case 0
+      
+      p = 1:n;
+      q = 1:n;
+
+      R = A;
+      for x=1:n-1
+        for y=x+1:n
+          R(y,x) = R(y,x)/R(x,x);
+          R(y,x+1:n) = R(y,x+1:n) - R(y,x)*R(x,x+1:n);
+        end
+      end
+
+    case 1
+      disp('Opcja 1')
+    case 2
+      disp('Opcja 2')
+    otherwise
+      error(sprintf('Opcja s=%d nie jest wspierana.', s))
+  end
 end
