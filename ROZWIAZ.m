3d7ebaeebb953636a57a948e7b3b4061781e1aed
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-05-25 21:29:35 +0200

    Usunięcie rozkładu R na LU w ROZWIAZ.m

--- ROZWIAZ.m
+++ ROZWIAZ.m
@@ -14,9 +14,6 @@ function [x] = ROZWIAZ(R, p, q, b)
 % Zobacz też: ROZKLAD
 
   n = size(R, 1);
-  L = tril(R, -1) + eye(n);
-  U = triu(R);
-
   P = eye(n); P = P(p,:);
   Q = eye(n); Q = Q(q,:);
   c = P*b;
@@ -24,14 +21,14 @@ function [x] = ROZWIAZ(R, p, q, b)
   % Rozwiązujemy Lz = c
   z(1) = c(1);
   for i=2:n
-    z(i) = c(i) - L(i,1:i-1)*z(1:i-1)';
+    z(i) = c(i) - R(i,1:i-1)*z(1:i-1)';
   end
   z = z';
 
   % Rozwiązujemy Uy = z
-  y(n) = z(n)/U(n,n);
+  y(n) = z(n)/R(n,n);
   for i=n-1:-1:1
-    y(i) = (z(i) - U(i,i+1:n)*y'(i+1:n))/U(i,i);
+    y(i) = (z(i) - R(i,i+1:n)*y'(i+1:n))/R(i,i);
   end
   y = y';
 
