c05c625e43906780de45357b0271885041882b1f
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-06-01 02:21:02 +0200

    Test poprawności dla macierzy "jedynkowych"

---
+++ test_error_matrix1_matrices.m
@@ -0,0 +1,43 @@
+N = [50 54 55 60 70 80 100 125 150];  % Rozmiar macierzy
+
+rel_matlab = zeros(size(N, 2), 1);
+rel_bez_gl = zeros(size(N, 2), 1);
+rel_gl_kol = zeros(size(N, 2), 1);
+rel_gl_mat = zeros(size(N, 2), 1);
+
+for i=1:size(N, 2)
+
+    A = matrix1(N(i));
+    x = ones(N(i), 1);
+    b = A*x;
+
+    rel_matlab(i) = rel_error(x, A\b);
+
+    [R, p, q] = ROZKLAD(A, 0);
+    x_bez_gl = ROZWIAZ(R, p, q, b);
+    rel_bez_gl(i) = rel_error(x, x_bez_gl);
+
+    [R, p, q] = ROZKLAD(A, 1);
+    x_gl_kol = ROZWIAZ(R, p, q, b);
+    rel_gl_kol(i) = rel_error(x, x_gl_kol);
+
+    [R, p, q] = ROZKLAD(A, 2);
+    x_gl_mat = ROZWIAZ(R, p, q, b);
+    rel_gl_mat(i) = rel_error(x, x_gl_mat);
+
+end
+
+values = [N', rel_matlab, rel_bez_gl, rel_gl_kol, rel_gl_mat];
+
+hold on
+  grid on
+  semilogy(N, rel_matlab);
+  semilogy(N, rel_bez_gl);
+  semilogy(N, rel_gl_kol);
+  semilogy(N, rel_gl_mat);
+  xlabel('Rozmiar macierzy');
+  ylabel('Błąd względny');
+  %title('Porównanie błędów względnych rozwiązania układu równań danego macierzą "jedynkową"');
+  legend('Matlab', 'Gauss bez el. głównego', 'Z el. głównym w kolumnie', 'Z el. głównym w macierzy');
+  print -depscairo '-S450, 300' 'test_error_matrix1_matrices.eps'
+hold off
